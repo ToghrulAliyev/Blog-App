e44eb6925de5f14761205a9db81fcc4f68831187
ToghrulAliyev <togrulali4@gmail.com>
2023-10-07 17:53:08 +0400

    cleaning

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../../modules/context/UserContext";
 
